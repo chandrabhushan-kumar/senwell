7ec1351c10619d15ef82f10c3892916a88b9ab32
chandrabhushan-kumar <kumar30895@gmail.com>
2022-10-04 14:32:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import * as React from 'react';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+export default function App() {
+  const [data, SetData] = useState([
+    {
+      
+      name: 'Sagar',
+    },
+    {
+      name: 'Suraj',
+    },
+    {
+      name: 'Suraj',
+    },
+    {
+    
+      name: 'chandra',
+    },
+    {
+     
+      name: 'Ranjan',
+    },
+    {
+     
+      name: 'Suraj',
+    },
+    
+    {
+      name: 'Suraj',
+    },
+    {
+      name: 'Suraj',
+    },
+    {
+      name: 'Suraj',
+    },
+    {
+     
+      name: 'Sagar',
+    },
+    {
+    
+      name: 'chandra',
+    },
+    {
+     
+      name: 'Ranjan',
+    },
+    {
+     
+      name: 'Suraj',
+    },
+    {
+     
+      name: 'Suraj',
+    },
+
+    {
+      name: 'Suraj',
+    },
+    {
+      name: 'chandra',
+    },
+    {
+      name: 'Suraj',
+    },
+    {
+      name: 'Suraj',
+    },
+    {
+      name: 'chandra',
+    },
+  ]);
+  const [shortedData, setShortedData] = useState([]);
+  useEffect(() => {
+     mock();
+    //  countNumberOfInstance(data,"name");
+    //  console.log(" ", countNumberOfInstance(data,"name"))
+  }, []);
+
+  const mock = () => {
+    var newArr = countNumberOfInstance(data, 'name');
+    setShortedData(newArr);
+  //  console.log('abchchc ', newArr);
+  };
+
+  const countNumberOfInstance = (arr, key) => {
+    var arr2 = [];
+    arr.forEach((x) => {
+      
+      if (
+        arr2.some((val) => {
+          return val[key] == x[key];
+        })
+      ) {
+       
+        arr2.forEach((k) => {
+          if (k[key] === x[key]) {
+            k['occurrence']++;
+          }
+        });
+      } else {
+       
+        let a = {};
+        a[key] = x[key];
+        a['occurrence'] = 1;
+        arr2.push(a);
+      }
+    });
+   
+
+    return arr2;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+     
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Count</th>
+        </tr>
+        {shortedData && shortedData.length > 0
+          ? shortedData.map((value, index) => {
+              let bgColor = '';
+              value.occurrence < 3
+                ? (bgColor = 'bgRed')
+                : value.occurrence > 2 && value.occurrence < 10
+                ? (bgColor = 'bgYellow')
+                : (bgColor = 'bgGreen');
+              return (
+                <tr key={index} className={bgColor}>
+                  <td>{value.name}</td>
+                  <td>{value.occurrence}</td>
+                </tr>
+              );
+            })
+          : null}
+      </table>
     </div>
   );
 }
-
-export default App;
